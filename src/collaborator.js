e22de3d6c4e6e1e5ce299fa7a672cc37dbf5bb90
Anjum Qureshi <anjumqureshi000@gmail.com>
2018-09-09 01:14:31 +0530

    removing collaborators can also be stored and piped

--- src/collaborator.js
+++ src/collaborator.js
@@ -3,56 +3,6 @@ const Table = require('cli-table');
 const token = process.env.GIT_API_TOKEN;
 const chars = require('./utils.js').chars;
 
-const statusCodes = {
-    204: (res, content) => {
-        return content;
-    },
-
-    404: () => {
-        return "you do not have admin priveleges for this repository";
-    },
-
-    201: (res) => {
-        let table = new Table({
-            chars: chars,
-            head: ['Id', 'Repository', 'Invitee', 'Inviter', 'Permission', 'Date'],
-            colWidths: [10, 25, 25, 25, 10, 25]
-        });
-        let invitation = JSON.parse(res.data);
-        let row = [
-            invitation.id, invitation.repository.full_name,
-            invitation.invitee.login, invitation.inviter.login,
-            invitation.permissions, invitation.created_at
-        ];
-        table.push(row);
-        return table.toString();
-    }
-};
-
-const printOuput = function (vorpal, res, callback) {
-    res.setEncoding('utf8');
-    let data = '';
-    res.on('data', (chunk) => (data += chunk));
-    res.on('end', () => {
-        res.data = data;
-        let output = statusCodes[res.statusCode](res, res.message);
-        vorpal.log(output);
-        callback();
-    });
-};
-
-const request = function (message, options, callback) {
-    let vorpal = this;
-    let req = http.request(options, (res) => {
-        res.message = message;
-        printOuput(vorpal, res, callback);
-    });
-    req.on('error', (error) => {
-        vorpal.log(`problem with request: ${error.message}`);
-    });
-    req.end();
-};
-
 const options = {
     host: 'api.github.com',
     headers: {
@@ -61,6 +11,91 @@ const options = {
         "Content-Length": 0,
         "Authorization": "bearer " + token
     }
+};
+
+let messages = [], output = [];
+
+const createTableInvits = function() {
+    return new Table({
+            chars: chars,
+            head: ['Id', 'Repository', 'Invitee', 'Inviter', 'Permission', 'Date'],
+            colWidths: [10, 25, 25, 25, 10, 25]
+        });
+};
+
+const formatTable = (invitation) => {
+    return [
+        invitation.id, invitation.repository.full_name,
+        invitation.invitee.login, invitation.inviter.login,
+        invitation.permissions, invitation.created_at
+    ];
+};
+
+const printOuputTable = function() {
+    let table = createTableInvits();
+    table.push.apply(table,output.map(formatTable));
+    this.log(table.toString());
+    output = [];
+    messages =[];
+}; 
+
+const printOutput = function() {
+    let table = new Table({
+        chars: chars,
+        head:['Sl No.','Message'],
+        colWidths: [8,60]
+    });
+    table.push.apply(table,messages.map((msg,index)=>([index,msg])));
+    this.log(table.toString());
+    output = [];
+    messages =[];
+};
+
+
+const httpRequest = function (message,options) {
+    return new Promise(function(resolve, reject) {
+        var req = http.request(options, function(res) {
+            res.message = message;
+            res.setEncoding('utf8');
+            let data = '';
+            if(res.statusCode == 404) {
+                return reject(Error("you donot have admin priveleges for this repo"));
+            }
+            if(res.statusCode == 204) {
+                messages.push(res.message)
+                resolve(data);
+                return;
+            }
+            res.on("data",(chunk)=>(data += chunk));
+            res.on("end",()=>{
+                data = JSON.parse(data);
+                output.push(data);
+                resolve(data);
+            });
+
+        });
+        req.on('error', (error) => {
+            reject(error);
+        });
+        req.end();
+    });
+};
+
+const addCollaborator = function (args, callback) {
+    options.method = 'PUT';
+    let promises = [];
+    args.collab.forEach(collab => {
+        options.path = `/repos/${args.org}/${args.repo}/collaborators/${collab}`;
+        const message = `${collab} is already a collaborator on ${args.repo}`;
+        promises.push(httpRequest(message, options));
+    });
+    Promise.all(promises)
+        .then(printOuputTable.bind(this))
+        .then(callback)
+        .catch((error)=>{
+            this.log(error.message);
+            callback();
+        });
 };
 
 const validateArgsArePresent = function (args) {
@@ -77,23 +112,25 @@ const validateArgsArePresent = function (args) {
     return this.chalk.red("pass all the options\n type help or [command --help] ");
 };
 
-const addCollaborator = function (args, callback) {
-    options.method = 'PUT';
-    args.collab.forEach(collab => {
-        options.path = `/repos/${args.org}/${args.repo}/collaborators/${collab}`;
-        const message = `${collab} is already a collaborator on ${args.repo}`;
-        request.call(this, message, options, callback);
-    });
-};
+
 
 const removeCollaborator = function (args, callback) {
     options.method = 'DELETE';
+    let promises = [];
     args.collab.forEach(collab => {
         options.path = `/repos/${args.org}/${args.repo}/collaborators/${collab}`;
         const message = `${collab} removed from ${args.repo} as collaborator`;
-        request.call(this, message, options, callback);
+        promises.push(httpRequest(message, options));
     });
-}
+    Promise.all(promises)
+        .then(printOutput.bind(this))
+        .then(callback)
+        .catch((error)=>{
+            this.log(error);
+            callback();
+        });
+};
+
 
 module.exports = {
     addCollaborator: addCollaborator,
